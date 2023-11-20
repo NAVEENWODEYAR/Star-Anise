acdd1f9b4f1d8a4b26a42943b3c30b6a907e1f35
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-20 14:40:35 +0530

    lunch break

---
+++ src/main/java/com/bhas/BhaskaraApplication.java
@@ -0,0 +1,13 @@
+package com.bhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhaskaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhaskaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhas/BhaskaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhaskaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

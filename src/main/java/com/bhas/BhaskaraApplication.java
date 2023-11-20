5ce28abeb2d2a73b4ca86e9f9a4536717c559821
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-20 14:44:58 +0530

    public class ServletInitializer extends SpringBootServletInitializer

--- src/main/java/com/bhas/BhaskaraApplication.java
+++ src/main/java/com/bhas/BhaskaraApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BhaskaraApplication {
+public class BhaskaraApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(BhaskaraApplication.class, args);
+		System.out.println(BhaskaraApplication.class);
 	}
 
 }

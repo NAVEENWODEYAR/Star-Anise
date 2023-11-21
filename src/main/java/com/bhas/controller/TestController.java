bbc9d87368eaff321035bd855e879a3c91b9e4a5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-21 22:37:19 +0530

    @ResponseStatus(code = HttpStatus.CONTINUE)

--- src/main/java/com/bhas/controller/TestController.java
+++ src/main/java/com/bhas/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.bhas.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController
 {
 	@GetMapping
+	@ResponseStatus(code = HttpStatus.CONTINUE)
 	public String greetMsg()
 	{
 		System.out.println("\n*********");

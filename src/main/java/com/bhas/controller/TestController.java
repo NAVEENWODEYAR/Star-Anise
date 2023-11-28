7aa8b74edd51f0919d55ca2a22147776339b3fc4
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-28 16:14:52 +0530

    org.springframework.beans.factory.BeanCreationException

--- src/main/java/com/bhas/controller/TestController.java
+++ src/main/java/com/bhas/controller/TestController.java
@@ -6,13 +6,15 @@ import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController
 {
-	@GetMapping
+	@GetMapping("/test")
 	@ResponseStatus(code = HttpStatus.CONTINUE)
 	public String greetMsg()
 	{
@@ -20,7 +22,8 @@ public class TestController
 		return "Hello Welcome to Bhaskara Application,";
 	}
 	
-	@GetMapping("/today")
+//	@GetMapping("/today")
+	@RequestMapping(name = "today",method = RequestMethod.GET)
 	public ResponseEntity<LocalDateTime> getDate()
 	{
 		return new ResponseEntity<>(LocalDateTime.now(), HttpStatus.OK);

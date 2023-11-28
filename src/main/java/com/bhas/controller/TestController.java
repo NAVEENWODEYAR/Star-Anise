7c8b6c6f75d8e48e9f80d74637562a19275c971e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-28 16:18:26 +0530

    Whitelabel Error Page

--- src/main/java/com/bhas/controller/TestController.java
+++ src/main/java/com/bhas/controller/TestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/bhaskara")
 public class TestController
 {
 	@GetMapping("/test")

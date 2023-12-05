7e0bc6e1a7e99cfa61040a4c571b062e2bfab898
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-05 22:26:08 +0530

    logOut

--- src/main/java/com/bhas/controller/TestController.java
+++ src/main/java/com/bhas/controller/TestController.java
@@ -5,8 +5,6 @@ import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 

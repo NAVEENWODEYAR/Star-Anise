e12c93ebbc22a57e7cee2f3be7428f1638625fed
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-06 15:32:03 +0530

    trying to resolve the ticket

--- src/main/java/com/bhas/controller/TestController.java
+++ src/main/java/com/bhas/controller/TestController.java
@@ -2,15 +2,25 @@ package com.bhas.controller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bhas.dto.FlightBookingAcknowledgment;
+import com.bhas.dto.FlightBookingRequest;
+import com.bhas.service.FlightBookingService;
 
 @RestController
 public class TestController
 {
+	@Autowired
+	private FlightBookingService service;
+	
 	@GetMapping("/test")
 	@ResponseStatus(code = HttpStatus.CONTINUE)
 	public String greetMsg()
@@ -24,5 +34,11 @@ public class TestController
 	public ResponseEntity<LocalDateTime> getDate()
 	{
 		return new ResponseEntity<>(LocalDateTime.now(), HttpStatus.OK);
+	}
+	
+	@PostMapping("/bookFlight")
+	public FlightBookingAcknowledgment bookFlightTicket(@RequestBody FlightBookingRequest request)
+	{
+		return service.bookFlightTicket(request);
 	}
 }
